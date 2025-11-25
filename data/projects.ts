6c474b3cdab7a290ae82df67dbade015ef4695a3
Farhan Nugraha <farhanoegrahaa@gmail.com>
2025-11-25 22:30:35 +0700

    feat(data) : add updated entries to projects, blog, and works

---
+++ data/projects.ts
@@ -0,0 +1,38 @@
+const projects = [
+  {
+    slug: "project-A",
+    title: "DeepCare : AI-Powered Medical App",
+    image: "/public/project-A/projectA.jpg",
+    description:
+      "DeepCare is a mobile AI-powered medical app that helps healthcare staff predict patient conditions.",
+    startDate: "2023-10",
+    endDate: "2024-01",
+    techStack: [
+      "Android Studio",
+      "Kotlin",
+      "Retrofit",
+      "Jetpack Compose",
+      "Google Cloud",
+      "MySQL",
+    ],
+  },
+  {
+    slug: "project-B",
+    title: "Brain Tumor Detection System",
+    image: "/public/project-A/projectB.jpg",
+    description:
+      "A deep learning–based system designed to detect brain tumors from MRI images with high accuracy.",
+    startDate: "2024-11",
+    endDate: "2025-02",
+    techStack: [
+      "Python",
+      "Google Colab",
+      "Deep Learning",
+      "Tensorflow",
+      "Keras",
+      "VGG_16",
+    ],
+  },
+];
+
+export default projects;
