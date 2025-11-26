431fa6392687a58cb18e21ee7eb480e782122ce0
Farhan Nugraha <farhanoegrahaa@gmail.com>
2025-11-27 00:28:46 +0700

    feat(blog): add blog.mdx data and update breadcrumbs structure

--- app/experience/WorkNotFound.tsx
+++ app/experience/WorkNotFound.tsx
@@ -14,7 +14,7 @@ export default function WorkNotFound() {
         href="/experience"
         className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-2 rounded transition-colors shadow focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
       >
-        <FaArrowLeft className="inline-block mr-2" /> Back to /work
+        <FaArrowLeft className="inline-block mr-2" /> Back to /Experience
       </Link>
     </div>
   );
