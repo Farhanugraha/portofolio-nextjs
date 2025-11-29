7d860eef078e72b2958de1e762be3ea4067ccd93
Farhan Nugraha <farhan.14092003@gmail.com>
2025-11-29 21:14:29 +0700

    (data): add .mdx data ,photo and update projects.ts metadata

--- data/projects.ts
+++ data/projects.ts
@@ -33,6 +33,43 @@ const projects = [
       "VGG_16",
     ],
   },
+  {
+    slug: "Bsbproject",
+    title: "Digital Document Management",
+    image: "/project-A/projectC.jpg",
+    description:
+      "A centralized digital document management system for Bank Sumsel Babel with secure workflows and role-based access control.",
+    startDate: "2023-07",
+    endDate: "2023-08",
+    techStack: [
+      "PHP",
+      "Laravel",
+      "MySQL",
+      "Bootstrap",
+      "Blade",
+      "Eloquent ORM",
+    ],
+  },
+  {
+    slug: "Bsproject",
+    title: "BSP Internal System",
+    image: "/project-A/projectD.jpg",
+    description:
+      "An internal system for Bumi Sawit Indo Permai to monitor and manage operational data for improved oversight.",
+    startDate: "2025-04",
+    endDate: "2025-05",
+    techStack: ["PHP", "MySQL", "Bootstrap"],
+  },
+  {
+    slug: "Digrepoproject",
+    title: "Unsri Research Digital Repository",
+    image: "/project-A/projectE.jpg",
+    description:
+      "A digital repository platform for Universitas Sriwijaya to store, organize, and provide structured access to research documents",
+    startDate: "2024-03",
+    endDate: "2024-04",
+    techStack: ["PHP", "Laravel", "MySQL", "Bootstrap", "Blade"],
+  },
 ];
 
 export default projects;
